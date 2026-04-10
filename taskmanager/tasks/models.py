c34b398f9e06d7b8ed75f89fbaef905fa358eebb
Juliano Urias <juliano.urias@encora.com>
2026-04-10 09:57:18 -0300

    feat: task crud with django templates

---
+++ taskmanager/tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('done', 'Done'),
+]
+
+class Task(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default="")
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
