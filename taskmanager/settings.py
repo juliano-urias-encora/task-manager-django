1e969b9cebcca46f128c5c0d1d25109b4c23805c
Juliano Urias <juliano.urias@encora.com>
2026-04-16 09:55:48 -0300

    chore: ALLOWED_HOSTS

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 
 # Application definition
